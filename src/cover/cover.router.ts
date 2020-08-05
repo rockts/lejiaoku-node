53a14274b576eaeee1117b48a7fccfcb138e2e59
rockts <rockts@sina.com>
2020-08-06 03:44:54 +0800

    上传文件成功

--- src/cover/cover.router.ts
+++ src/cover/cover.router.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import * as coverController from './cover.controller';
 import { authGuard } from '../auth/auth.middleware';
-import { coverInterceptor, coverProcessor } from './cover.middleware';
+import { coverInterceptor } from './cover.middleware';
 
 const router = express.Router();
 
@@ -12,19 +12,19 @@ router.post(
   '/covers',
   authGuard,
   coverInterceptor,
-  coverProcessor,
+  // coverProcessor,
   coverController.store,
 );
 
-/**
- * 文件服务
- */
-router.get('/covers/:coverId/serve', coverController.serve);
+// /**
+//  * 文件服务
+//  */
+// router.get('/covers/:coverId/serve', coverController.serve);
 
-/**
- * 文件信息
- */
-router.get('/covers/:coverId/metadata', coverController.metadata);
+// /**
+//  * 文件信息
+//  */
+// router.get('/covers/:coverId/metadata', coverController.metadata);
 
 /**
  * 导出路由
