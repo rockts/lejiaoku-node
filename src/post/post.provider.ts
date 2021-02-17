d98e479e6311fdd86d5f127984b8613598c99a23
rockts <rockts@sina.com>
2021-02-17 11:51:08 +0800

    add sql filename

--- src/post/post.provider.ts
+++ src/post/post.provider.ts
@@ -15,107 +15,109 @@ export const sqlFragment = {
     LEFT JOIN avatar
       ON user.id = avatar.userId
   `,
-    //     category: `
-    //     (
-    //       SELECT
-    //         category.name
-    //       FROM
-    //         category
-    //       WHERE category.id = post.categoryId
-    //     ) AS category
-    //   `,
+    category: `
+        (
+          SELECT
+            category.name
+          FROM
+            category
+          WHERE category.id = post.categoryId
+        ) AS category
+      `,
+
     cover: `
     CAST(
-      IF(
+    IF(
         COUNT(cover.id),
         GROUP_CONCAT(
-          DISTINCT JSON_OBJECT(
-            'id', cover.id,
-            'width', cover.width,
-            'height', cover.height
-          )
+            DISTINCT JSON_OBJECT(
+                'id', cover.id,
+                'width', cover.width,
+                'height', cover.height
+            )
         ),
         NULL
-      ) AS JSON
-    ) AS cover
-  `,
+    ) AS JSON
+) AS cover
+    `,
     leftJoinOneCover: `
-    LEFT JOIN LATERAL (
-      SELECT *
-      FROM 
+LEFT JOIN LATERAL(
+    SELECT *
+    FROM 
         cover
       WHERE cover.postId = post.id
       ORDER BY cover.id DESC
       LIMIT 1
-    ) AS cover ON post.id = cover.postId
-  `,
+) AS cover ON post.id = cover.postId
+    `,
     totalComments: `
     (
-      SELECT
+        SELECT
         COUNT(comment.id)
       FROM
         comment
       WHERE
         comment.postId = post.id
     ) AS totalComments
-  `,
+        `,
     leftJoinOneFile: `
-    LEFT JOIN LATERAL (
-      SELECT *
-      FROM file
+LEFT JOIN LATERAL(
+    SELECT *
+    FROM file
       WHERE file.postId = post.id
       ORDER BY file.id DESC
       LIMIT 1
-    ) AS file ON post.id = file.postId
-  `,
+) AS file ON post.id = file.postId
+    `,
     file: `
-    CAST(
-      IF(
+CAST(
+    IF(
         COUNT(file.id),
         GROUP_CONCAT(
-          DISTINCT JSON_OBJECT(
-            "id", file.id,
-            "mimetype", file.mimetype,
-            "size", file.size
-          )
+            DISTINCT JSON_OBJECT(
+                "id", file.id,
+                "filename", file.originalname,
+                "mimetype", file.mimetype,
+                "size", file.size
+            )
         ),
         NULL
-      ) AS JSON
-    ) AS file
-  `,
+    ) AS JSON
+) AS file
+    `,
     leftJoinTag: `
-    LEFT JOIN
-    post_tag ON post_tag.postId = post.id
-    LEFT JOIN
-      tag ON post_tag.tagId = tag.id
-  `,
+LEFT JOIN
+post_tag ON post_tag.postId = post.id
+LEFT JOIN
+tag ON post_tag.tagId = tag.id
+    `,
     tags: `
-    CAST(
-      IF(
+CAST(
+    IF(
         COUNT(tag.id),
         CONCAT(
-          '[',
+            '[',
             GROUP_CONCAT(
-              DISTINCT JSON_OBJECT(
-                'id', tag.id,
-                'name', tag.name
-              )
+                DISTINCT JSON_OBJECT(
+                    'id', tag.id,
+                    'name', tag.name
+                )
             ),
-          ']'
+            ']'
         ),
         NULL
-      ) AS JSON
-    ) AS tags
-  `,
+    ) AS JSON
+) AS tags
+    `,
     totalLikes: `
     (
-      SELECT COUNT(user_like_post.postId)
+        SELECT COUNT(user_like_post.postId)
       FROM user_like_post
       WHERE user_like_post.postId = post.id
     ) AS totalLikes
-  `,
+        `,
     innerJoinUserLikePost: `
-    INNER JOIN user_like_post
-      ON user_like_post.postId = post.id
-  `,
+INNER JOIN user_like_post
+ON user_like_post.postId = post.id
+    `,
 };
